194403a356444900351ca4a1de56ad150e2b4ad1
Dipesh <dipeshPC@dipeshs-mac-mini.local>
2016-02-10 16:37:44 +0530

    Initial Commit

---
+++ 1UP_Mobile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1UP_Mobile
+//
+//  Created by Dipesh on 10/02/16.
+//  Copyright © 2016 Dipesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1UP_Mobile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1UP_Mobile
+//
+//  Created by Dipesh on 10/02/16.
+//  Copyright © 2016 Dipesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
